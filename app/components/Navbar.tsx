53673de3cc0f6020ae05a440b65fc8880cceca6a
drili <DriliB@hotmail.com>
2023-12-17 23:40:51 +0100

    wip navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Link from 'next/link';
 
-const Navbar: React.FC = () => {
+interface MenuObject {
+    menuLink: string;
+    menuText: string;
+}
+
+interface NavbarProps {
+    menuItems: MenuObject[]
+}
+
+const Navbar: React.FC<NavbarProps> = ({ menuItems }) => {
   return (
-    <div>Navbar</div>
+    <div id='component_Navbar'>
+        <nav>
+            <ul>
+                {menuItems.map((item, index) => (
+                    <li key={index}>
+                        <Link href={item.menuLink}>
+                            {item.menuText}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import Image from 'next/image'
+import Navbar from './components/Navbar'
 
 export default function Home() {
+    const menuItems = [
+        { menuLink: '/link1', menuText: 'Link 1' },
+        { menuLink: '/link2', menuText: 'Link 2' },
+    ]
+
     return (
-        <h1>Hello World!</h1>
+        <div id='component_Home'>
+            <Navbar menuItems={menuItems} />
+
+            <div>
+                <h1>Home</h1>
+            </div>
+        </div>
     )
 }
